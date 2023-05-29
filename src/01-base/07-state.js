33185cdf0c7dabc1625fbedaa798eafc11fe816c
VesperSpoony <714694113@qq.com>
2023-05-29 08:47:21 +0800

    2023/5/29

--- src/01-base/07-state.js
+++ src/01-base/07-state.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App7 extends Component {
   state = {
-    myShow: true
-  }
+    myShow: true,
+  };
   render() {
     return (
       <div>
         <h1>Hello React</h1>
-        <button onClick={() => {
-          this.setState({
-            myShow: !this.state.myShow
-          })
-          if (this.state.myShow) {
-            console.log("收藏的逻辑");
-          } else {
-            console.log("取消的逻辑");
-          }
-        }}>{this.state.myShow ? '收藏' : '取消收藏'}</button>
-      </div >
-    )
+        <button
+          onClick={() => {
+            this.setState({
+              myShow: !this.state.myShow,
+            });
+            if (this.state.myShow) {
+              console.log("收藏的逻辑");
+            } else {
+              console.log("取消的逻辑");
+            }
+          }}
+        >
+          {this.state.myShow ? "收藏" : "取消收藏"}
+        </button>
+      </div>
+    );
   }
 }
